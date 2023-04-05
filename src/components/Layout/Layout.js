5022d98cc8ff4432f630f4dacf3a7a6db8896839
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-05 15:26:20 +0600

    inside Layout created png logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import Nav from "../Nav/Nav";
+import logo from "../../assets/logo.png"
 
 export default function Layout(props) {
   return(
     <div className="Layout">
     <header>
+      <img src={logo} alt="logo" />
       <Nav />
     </header>
     <aside>
