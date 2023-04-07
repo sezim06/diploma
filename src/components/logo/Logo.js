94c185f342e54848a3d5eeb2fb5e1602ad38a0fe
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-07 22:00:47 +0600

    added the Logo.js file and wrote the code for the logo

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,10 @@
+import "./Logo.css"
+import logo from "../../assets/logo.png"
+export default function Logo(){
+  return (
+    <div className="Logo">
+      <img src={logo} alt="logo" className="logo"/>
+
+    </div>
+  )
+}
