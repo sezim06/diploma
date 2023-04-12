a2a83736052cdc8d6715e908a4fe458b0a9b10be
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-12 14:50:12 +0600

    deleted document firebase

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,28 +1,12 @@
-import { getDocs } from "firebase/firestore/lite";
+
 import { NavLink } from "react-router-dom";
-import { categories } from "../../firebase";
-import { useEffect, useState } from "react";
 import "./CategoryList.css"
+import { useContext } from "react";
+import { AppContext } from "../../App";
 
 export default function CategoryList() {
-  const [categoryList, setCategoryList] = useState([]);
-
-  // получить документы из списка категорий в Firebase.
-  useEffect(() => {
-    getDocs(categories).then(snapshot => {
-      const newCategoryList = [];
-      snapshot.docs.forEach(doc => {
-        const category = doc.data(); // { name: "...", slug: "..." }
-        category.id = doc.id;
-  
-        newCategoryList.push(category);
-      })
-  
-      setCategoryList(newCategoryList);
-    });
-  }, []);
-
-  const output = categoryList.map(category => (
+ const { categories } = useContext(AppContext);
+const output = categories.map(category => (
     <li key={category.id}>
       <NavLink to={`/categories/${category.slug}`}>
         {category.name}
