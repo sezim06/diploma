ddde4fe8b2db8c49f21a0b3307414f3cee3d8fd1
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-14 15:02:39 +0600

    added new file and add product img

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -3,18 +3,21 @@ import "./ProductsList.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
 
-export default function ProductsList() {
+export default function ProductsList({category}) {
   const { products } = useContext(AppContext);
 
-  const output = products.map(product => (
-    <div key={product.slug}>
+  const output = products.map(product => product.category === category.id) 
+    .map(product => (
+      <div key={product.id} className="Product">
       <img src={product.picture} alt={product.name}/>
       <NavLink to={'/product' + product.slug}>
         {product.name}
       </NavLink>
       <span>{product.price} som</span>
     </div>
-  ))
+    ));
+    
+  
 
   return (
     <div className="ProductsList">
