06b66ecc94b5c3970efe11c3cd58aa025b638533
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-18 20:26:05 +0600

    formated all code

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,25 +1,24 @@
-import { useMatch } from "react-router-dom"
+import { useMatch } from "react-router-dom";
 import { useContext } from "react";
-import { AppContext } from "../App"
+import { AppContext } from "../App";
 import NotFound from "./NotFound";
+import "./Product.css"
 
 export default function Product() {
-  const { params } = useMatch("/products/:slug")
-  const { products } = useContext(AppContext)
+  const { params } = useMatch("/product/:slug");
+  const { products } = useContext(AppContext);
 
-  const product = products.find(product => product.slug === params.slug);
+  const product = products.find((product) => product.slug === params.slug);
 
   if (!product) {
-    return <NotFound />
+    return <NotFound />;
   }
 
-
-
-  return(
+  return (
     <div className="Product">
       <h1>{product.name}</h1>
-      <img src={products.picture} alt="product.name" />
+      <img src={product.picture} alt={product.name} />
       <span>{product.price}</span>
     </div>
-  )
+  );
 }
