bb7ea2df103cae8c76eb27b3455233f5a69afb35
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-18 20:24:33 +0600

    change insude cart add number product

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -2,26 +2,21 @@ import React, { useContext } from "react";
 import "./ProductsList.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+import AddToCart from "../AddToCart/AddToCart";
 
-export default function ProductsList({category}) {
+export default function ProductsList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.map(product => product.category === category.id) 
-    .map(product => (
+  const output = products
+    .filter ((product) => product.category === category.id)
+    .map((product) => (
       <div key={product.id} className="Product">
-      <img src={product.picture} alt={product.name}/>
-      <NavLink to={'/product' + product.slug}>
-        {product.name}
-      </NavLink>
-      <span>{product.price} som</span>
-    </div>
+        <img src={product.picture} alt={product.name} className="imageProduct" />
+        <NavLink to={"/product/" + product.slug}>{product.name}</NavLink>
+        <span>{product.price} som</span>
+        <AddToCart product={product} />
+      </div>
     ));
-    
-  
 
-  return (
-    <div className="ProductsList">
-      {output}
-    </div>
-  )
+  return <div className="ProductsList">{output}</div>;
 }
