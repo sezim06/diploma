9ef779b28d5fc8c31f7add34a65f5b4a31fdb620
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-18 20:20:28 +0600

    created icon for category

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -8,6 +8,7 @@ export default function CategoryList() {
  const { categories } = useContext(AppContext);
 const output = categories.map(category => (
     <li key={category.id}>
+      <img src={category.icon} alt={category.name} className="icon" />
       <NavLink to={`/categories/${category.slug}`}>
         {category.name}
       </NavLink>
