2b631b8b11fa54f91114ba7b1e59034de02c7dff
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-19 21:53:06 +0600

    added order forms

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,11 @@
 import CartList from "../components/CardList/CardList";
-
+import OrderForm from "../components/OrderForm/OrderForm";
+import"./Pages.css"
 export default function Cart(){
   return(<div className="Cart">
-    <h1>Cart page</h1>
+    <h1><span>P</span>urchases</h1>
 
     <CartList/>
+    <OrderForm />
   </div>)
 }
