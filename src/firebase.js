50bece2c917bd930672d1617a08612e45da6558b
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-19 21:49:18 +0600

    linking firebase to cart

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from 'firebase/firestore/lite'
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +19,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Инициализация базы данных
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 // Получение списка категорий (коллекции документов)
 export const categoryCollection = collection(db, 'categories');
 export const productsCollection = collection(db, 'products');
+export const ordersCollection = collection(db, 'orders');
+
+const provider = new GoogleAuthProvider();
+export const signIn = () => signInWithPopup(auth, provider);
+export const signOff = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
