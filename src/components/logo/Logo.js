0e2a622a1d92face138303bbda97001e7e16d441
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-23 22:21:44 +0600

    changed logo icon

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,9 +1,10 @@
 import "./Logo.css"
-import logooo from "../../assets/logooo.png"
+import loggo from "../../assets/loggo.png"
 export default function Logo(){
   return (
     <div className="Logo">
-      <img src={logooo} alt="logo0" className="logo"/>
+      <img src={loggo} alt="logo" className="logo" />
+      
 
     </div>
   )
