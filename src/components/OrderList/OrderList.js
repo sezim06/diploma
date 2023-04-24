d4da9c0b9f7a0759c46e615fddca6d459befa37f
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-24 15:39:30 +0600

    create new folder and add many code

---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import "./OrderList.css";
+
+export default function OrderList() {
+  const { orders, products } = useContext(AppContext);
+
+  if (!orders.length || !products.length) {
+    return "No orders found.";
+  }
+
+  const output = orders.map(order => {
+    const cartOutput = Object.keys(order.cart).map(productId => {
+      const product = products.find(product => product.id === productId);
+
+      return (
+        <div>
+          {product.name}: {order.cart[productId]} X {product.price} som = {order.cart[productId] * product.price} som
+        </div>
+      );
+    })
+
+    return (
+      <div className="Order">
+        <div><span>Name:</span> {order.name}</div>
+        <div>Phone: {order.phone}</div>
+        <div>Address: {order.address}</div>
+        <div>Cart: {cartOutput}</div>
+      </div>
+    );
+  })
+
+  return (
+    <div className="OrderList">
+      {output}
+    </div>
+  );
+}
