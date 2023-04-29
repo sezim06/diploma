49a7fc6ad36e20763d0ae789a7bc2bd86651fbcb
sezim06 <sezimaliaskarova352@gmail.com>
2023-04-29 17:02:02 +0600

    changed same code and add new code too

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore/lite";
-import { categoryCollection, onAuthChange, ordersCollection, productsCollection } from "./firebase";
+import { getDocs } from "firebase/firestore";
+import {
+  categoryCollection,
+  onAuthChange,
+  onCategoriesLoad,
+  ordersCollection,
+  productsCollection,
+} from "./firebase";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import ThankYou from "./pages/ThankYou";
 import Contact from "./pages/Contact";
 import Books from "./pages/Books";
 
-
-import "./App.css"
+import "./App.css";
 import Delivery from "./pages/Delivery";
 import Orders from "./pages/Orders";
 
@@ -21,7 +26,7 @@ import Orders from "./pages/Orders";
 export const AppContext = createContext({
   categories: [],
   products: [],
-  orders: [], 
+  orders: [],
   // контекст для корзины
   cart: {}, // содержимое корзинки
   setCart: () => {}, // изменить содержимое корзики
@@ -32,67 +37,71 @@ export const AppContext = createContext({
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
-  const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([]);
 
   const [cart, setCart] = useState(() => {
-    return JSON.parse(localStorage.getItem('cart')) || {};
+    return JSON.parse(localStorage.getItem("cart")) || {};
   });
 
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  useEffect(() => { // выполнить только однажды
-    getDocs(categoryCollection) // получить категории
-      .then(({ docs }) => { // когда категории загрузились
-        setCategories( // обновить состояние
-          docs.map(doc => ({ // новый массив
-            ...doc.data(), // из свойств name, slug
-            id: doc.id // и свойства id
-          }))
-        )
-      });
+  useEffect(() => {
+    // выполнить только однажды
+    onCategoriesLoad(setCategories);
 
     getDocs(productsCollection) // получить категории
-      .then(({ docs }) => { // когда категории загрузились
-        setProducts( // обновить состояние
-          docs.map(doc => ({ // новый массив
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setProducts(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
             ...doc.data(), // из свойств name, slug
-            id: doc.id // и свойства id
+            id: doc.id, // и свойства id
           }))
-        )
+        );
       });
-      getDocs(ordersCollection) // получить категории
-      .then(({ docs }) => { // когда категории загрузились
-        setOrders( // обновить состояние
-          docs.map(doc => ({ // новый массив
+    getDocs(ordersCollection) // получить категории
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setOrders(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
             ...doc.data(), // из свойств name, slug
-            id: doc.id // и свойства id
+            id: doc.id, // и свойства id
           }))
-        )
+        );
       });
 
-    onAuthChange(user => {
+    onAuthChange((user) => {
+      if (user) {
+        user.isAdmin = user.email === "sezimaliaskarova352@gmail.com";
+      }
       setUser(user);
     });
   }, []);
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products, cart, setCart, user, orders }}>
+      <AppContext.Provider
+        value={{ categories, products, cart, setCart, user, orders }}
+      >
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/books" element={<Books/>} />
-            <Route path="/contact" element={<Contact/>} />
-            <Route path="/delivery" element={<Delivery/>} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/delivery" element={<Delivery />} />
             <Route path="/categories/:slug" element={<Category />} />
-            <Route path="/products  /:slug" element={<Product />} />
+            <Route path="/products/:slug" element={<Product />} />
             <Route path="/thank-you" element={<ThankYou />} />
-            <Route path="/orders" element={<Orders/>} />
+            <Route path="/orders" element={<Orders />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
