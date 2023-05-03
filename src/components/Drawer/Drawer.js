fa84a25fe0d0968a06b257700dc6ce35135b0c70
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-03 20:51:22 +0600

    created new file drawer and add new code

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,20 @@
+
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import "./Drawer.css";
+
+function Drawer({ open, toggle }) {
+  const drawerClassNames = `Drawer ${open ? "open" : ""}`;
+
+  return (
+    <div className={drawerClassNames}>
+      <div onClick={toggle} className="backdrop"></div>
+      <div className="content">
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
