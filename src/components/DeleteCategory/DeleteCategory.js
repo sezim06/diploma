809752b35fa9b5b180a7e11f1a1a3285c31c12bc
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-03 20:50:38 +0600

    imported deleted category css

--- src/components/DeleteCategory/DeleteCategory.js
+++ src/components/DeleteCategory/DeleteCategory.js
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { AppContext } from "../../App";
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "../../firebase";
+import "./DeleteCategory.css"
+
 
 export default function DeleteCategory({ category }) {
   const { user, products } = useContext(AppContext);
