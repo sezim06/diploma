5e94c347bd139c3f96d78e04c89c8d6d89612409
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-03 20:52:07 +0600

    changed logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,9 +1,9 @@
 import "./Logo.css"
-import loggo from "../../assets/loggo.png"
+import logo from "../../assets/loggo.png"
 export default function Logo(){
   return (
     <div className="Logo">
-      <img src={loggo} alt="logo" className="logo" />
+      <img src={logo} alt="logo" className="logo" />
       
 
     </div>
