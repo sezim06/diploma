d3e079f7b731d65fef3111ef19809e3d15f2bc85
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-03 20:53:57 +0600

    chaged HOME.JS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,24 @@
 import "./Pages.css"
+
 export default function Home(){
-  return(
-    <div className="Home">
-      <h1>Welcome to My Page</h1>
-      <p>This is my home page. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget velit turpis. Sed pharetra laoreet sapien, sit amet efficitur ipsum venenatis a. Nulla eget augue sed lorem tempor suscipit at id arcu.</p>
-      <h2>My Interests</h2>
-      <ul>
-        <li>Reading</li>
-        <li>Books</li>
-        <li>Traveling</li>
-      </ul>
-      <h3>My Recent Projects</h3>
-      <ol>
-        <li>Created a personal website using React</li>
-        <li>Built a CRUD application using Node.js and MongoDB</li>
-        <li>Contributed to an open-source project on GitHub</li>
-      </ol>
-      
-        
-    </div>
-  )
+return(
+<div className="Home">
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/i-qT5n_5Mys" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+<h1>Welcome to My Page</h1>
+<p>This is my home page. Here you will find the best books, novels, recipes, crime books and thrillers.</p>
+<h2>My Interests</h2>
+<ul>
+<li>Reading</li>
+<li>Books</li>
+<li>Traveling</li>
+</ul>
+
+<h2>My Favorite Books</h2>
+<ul>
+<li>The Great Gatsby by F. Scott Fitzgerald</li>
+<li>To Kill a Mockingbird by Harper Lee</li>
+<li>Pride and Prejudice by Jane Austen</li>
+</ul>
+</div>
+)
 }
