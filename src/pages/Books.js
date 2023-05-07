868d9f9a161b712ae0b963139c4d21c135e7d611
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-07 16:07:07 +0600

    add information about books writing and changed code

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,8 +1,17 @@
 export default function Books(){
   return(
     <div className="Books">
-      <h1>Books</h1>
-      <p>This is my home page</p>
-    </div>
+    <h1>Books and Authors</h1>
+    <p>Explore the world of literature and the authors who create it.</p>
+    
+    <h2>Jane Austen</h2>
+    <p>Jane Austen (1775-1817) was an English novelist known for her witty social commentary and realistic portrayals of the lives of women in the Georgian era. Her most famous works include "Pride and Prejudice," "Sense and Sensibility," and "Emma."</p>
+    
+    <h2>Fyodor Dostoevsky</h2>
+    <p>Fyodor Dostoevsky (1821-1881) was a Russian novelist, philosopher, and journalist. His works explore the depths of the human psyche and the complexities of human nature. Some of his most famous works include "Crime and Punishment," "The Brothers Karamazov," and "Notes from Underground."</p>
+    
+    <h2>Toni Morrison</h2>
+    <p>Toni Morrison (1931-2019) was an American novelist, essayist, and professor. She was known for her powerful and poetic writing that explored themes of race, gender, and identity. Some of her most famous works include "Beloved," "The Bluest Eye," and "Song of Solomon."</p>
+  </div>
   )
 }
