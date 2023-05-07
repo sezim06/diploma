42018af417fd2d2738c7f57432c203bf9153e243
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-07 16:09:08 +0600

    added delivery changed code

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,8 +1,31 @@
-export default function Delivery(){
-  return(
+import "./Pages.css";
+export default function Delivery() {
+  return (
     <div className="Delivery">
       <h1>Delivery</h1>
-      <p>This is my home page</p>
+      <p>Here is some information about our delivery services.</p>
+  
+
+      <h2>Delivery Options</h2>
+      <p>
+        We offer a variety of delivery options to meet your needs, including
+        standard shipping, express shipping, and same-day delivery in select
+        areas.
+      </p>
+
+      <h2>Delivery Times</h2>
+      <p>
+        Our delivery times vary depending on the shipping method you choose and
+        your location. Standard shipping usually takes 3-5 business days, while
+        express shipping can arrive in as little as 1-2 business days.
+      </p>
+
+      <h2>Delivery Costs</h2>
+      <p>
+        Delivery costs are calculated based on the weight and dimensions of your
+        order, as well as the shipping method and destination. You can see the
+        exact delivery cost at checkout before you confirm your order.
+      </p>
     </div>
-  )
+  );
 }
