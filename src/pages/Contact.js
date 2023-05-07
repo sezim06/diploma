a7e2bb18247e501fadfda572a909873276815ebc
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-07 16:08:11 +0600

    added contact about us and some link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,18 @@
+import "./Pages.css"
 export default function Contact(){
   return(
-    <div className="Contact">
-      <h1>Contact</h1>
-      <p>This is my home page</p>
-    </div>
+    <div className="Contacts">
+    <h1>Contact Us</h1>
+    <p>Get in touch with us for any inquiries or feedback.</p>
+    
+    <h2>Email:</h2>
+    <p>Email us at <a href="https://mail.google.com/mail/u/0/#inbox">mail.google.com/Aliaskarova-Sezim/BookStore</a>.</p>
+    
+    <h2>Phone:</h2>
+    <p>Call us at +996 (500) 978-933.</p>
+    
+    <h2>Address:</h2>
+    <p>Visit us at 123 Main St, Anytown USA.</p>
+  </div>
   )
 }
