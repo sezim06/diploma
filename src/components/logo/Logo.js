9c295ebf683c9e6f396f93ba101a6bc8a8069c99
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-10 15:07:45 +0600

    added new chnages for file Logo.jsx inside main folder "Logo"

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,11 @@
 import "./Logo.css";
-import Logo from "../../assets/loggo.png";
+import llogo from "../../assets/loggo.png";
 import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <div className="Logo">
       <Link href="https://youtu.be/IaXjbgH3cZY">
-        <img src={Logo} alt="logo" className="logo" />
+        <img src={llogo} alt="logo" className="logo" />
       </Link>
     </div>
   );
