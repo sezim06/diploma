458a2a3c808c5aeab9fd2638e39e148d8c71021e
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-12 14:29:39 +0600

    add iframe and remove iframe

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,10 +1,13 @@
 import "./Pages.css";
+
 export default function Delivery() {
   return (
     <div className="Delivery">
+
+     
       <h1>Delivery</h1>
       <p>Here is some information about our delivery services.</p>
-  
+   {/* <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d9067.581208241209!2d74.57001254048367!3d42.86557983131667!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1683795271745!5m2!1sru!2skg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
 
       <h2>Delivery Options</h2>
       <p>
