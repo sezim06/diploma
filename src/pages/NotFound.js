cc5d3ce5913819d1e9621c53aa4a0111ddc1eb0b
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-12 14:30:14 +0600

    added animation for 404 not found and styled not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,42 @@
 export default function NotFound(){
   return(
     <div className="NotFound">
-      <h1>404</h1>
-      <h2>There are no books here</h2>
-     
+<div class="animation-404">
+  <h1>Oop<i>s</i></h1>
+  <div id="forklift-body">
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129.5 123">
 
+    <g class="tk" id="forklift">
+      <g id="tines">
+        <line x1="22" y1="78" x2="22" y2="113"/>
+        <line x1="22" y1="96.5" x2="0" y2="96.5"/>
+      </g>
+        <path class="cab" d="M51.5,46c1.3-0.6,6-1,7-1c2,0,6.7,6.6,5,8c-4.3,3.6-18.9,20.8-1,23h19h1c0-5.5,4.5-10,10-10h26 c6,0,7,2,7,7v26c0,5-3.6,9-8,9h-75l-14-18L28,89.5L50.6,46L68,12.5c0,0,3.5-5.5,14-6c0,0,28,0,28,0c2,0,6,2,6,6c0,0,0,54.5,0,52" />
+        <line class="cab" x1="68.7" y1="43.4" x2="61.7" y2="47.2"/>
+        <line class="cab" x1="65.7" y1="37.9" x2="71.7" y2="48.9"/>
+        <circle class="fl" cx="45.5" cy="104" r="18"/>
+        <circle class="fl" cx="104.3" cy="107.5" r="14.5"/>
+        <g class="tn">
+            <line class="cab" x1="111" y1="72.5" x2="119" y2="72.5"/>
+            <line class="cab" x1="111" y1="74.5" x2="119" y2="74.5"/>
+            <line class="cab" x1="111" y1="76.5" x2="119" y2="76.5"/>
+            <line x1="104.5" y1="66" x2="104.5" y2="93"/>
+            <path class="cab" d="M125.5,89c2,0,3-1,3-3v-3c0-2,0-3-3-3"  />
+            <path class="cab" d="M26,112.5h-2c-0.5,0-1-0.4-1-1V1.5c0-0.6,0.5-1,1-1h2c0.5,0,1,0.4,1,1v110 C27,112.1,26.5,112.5,26,112.5z"/>
+            <circle cx="45.5" cy="104" r="10.2"/>
+            <circle cx="104.3" cy="107.5" r="7.1"/>
+        </g>
+        <g class="fd">
+            <circle cx="45.5" cy="104" r="3.5"/>
+            <circle cx="104.3" cy="107.3" r="2.5"/>
+            <path  class="cab" d="M100.4,66.3l-16-2.2c-1.9-0.3-3.3-2-3-3.9v0c0.3-1.9,2-3.3,3.9-3l16,2.2c1.9,0.3,3.3,2,3,3.9l0,0 C104.1,65.2,102.3,66.6,100.4,66.3z"/>
+            <path class="cab" d="M113.3,39.6l-8.1,24.5c-0.6,1.8-2.6,2.8-4.4,2.2l0,0c-1.8-0.6-2.8-2.6-2.2-4.4l8.1-24.5 c0.6-1.8,2.6-2.8,4.4-2.2l0,0C112.9,35.8,113.9,37.8,113.3,39.6z"/>
+        </g>
+    </g>
+  </svg>
+  </div>
+</div>
+<p>Sorry about this — something's gone wrong at our end, but we're working hard to sort it out.</p>
     </div>
   )
 }
