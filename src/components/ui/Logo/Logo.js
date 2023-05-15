86dfa433d49d4e030ee6bc04fe8dde3315efcef9
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-15 16:07:21 +0600

    deleted a href

--- src/components/ui/Logo/Logo.js
+++ src/components/ui/Logo/Logo.js
@@ -1,12 +1,14 @@
 import "./Logo.css";
 import llogo from "../../../assets/loggo.png";
 import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
     <div className="Logo">
       <Link href="https://youtu.be/IaXjbgH3cZY">
         <img src={llogo} alt="logo" className="logo" />
       </Link>
+
     </div>
   );
 }
