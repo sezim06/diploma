1718197841e5ae1237bf5428122e25247f6ce633
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-15 20:51:31 +0600

    formated document

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import "./Pages.css";
+// import video from "../assets/video.mp4";
+
 
 export default function Home() {
   return (
@@ -12,24 +14,34 @@ export default function Home() {
           recipes, crime books and thrillers.
         </p>
         <a href="https://youtu.be/KwXyVh1zIhM">
-        <h1>My Interests</h1>
+          <h1>My Interests</h1>
         </a>
-        
-        <ul>
-          <a href="https://youtu.be/5wvgJePdZ7s"><li>Reading</li></a>
-          <a href="https://youtu.be/jxcMRkqaQdw"><li>Books</li></a>
-          <a href="https://youtu.be/TcgqDs-95Uw"><li>Traveling</li></a>
-        </ul>
-<a href="https://youtu.be/KwXyVh1zIhM">
-<h1>My Favorite Books</h1>
 
-</a>
+        <ul>
+          <a href="https://youtu.be/5wvgJePdZ7s">
+            <li>Reading</li>
+          </a>
+          <a href="https://youtu.be/jxcMRkqaQdw">
+            <li>Books</li>
+          </a>
+          <a href="https://youtu.be/TcgqDs-95Uw">
+            <li>Traveling</li>
+          </a>
+        </ul>
+        <a href="https://youtu.be/KwXyVh1zIhM">
+          <h1>My Favorite Books</h1>
+        </a>
         <ul>
           <li>The Great Gatsby by F. Scott Fitzgerald</li>
           <li>To Kill a Mockingbird by Harper Lee</li>
           <li>Pride and Prejudice by Jane Austen</li>
         </ul>
       </div>
+     
+      {/* <video widt5="400" height="300" controls>
+          <source src={video} type="video/mp4" />
+        </video> */}
+
     </div>
   );
 }
