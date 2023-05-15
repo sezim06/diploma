7a0970b2263c15434a25458db40cef941034ad7b
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-15 16:06:43 +0600

    added clasName and add new code for layout in cart

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -22,7 +22,7 @@ export default function Layout(props) {
         <Nav />
         <NavToggle callback={toggleDrawer} />
         <Drawer open={drawerOpen} toggle={toggleDrawer} />
-        <div>
+        <div className="right">
           <CartLink />
           <Auth />
         </div>
