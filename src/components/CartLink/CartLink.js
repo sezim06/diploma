5a857b736b47ad8802191b0f2d713f3379140f3e
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-19 15:05:32 +0600

    added  animation and changed class name

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -10,7 +10,7 @@ export default function CartLink() {
     <div className="CartLink">
       <NavLink to="/cart">
     
-        <svg className="cart"
+        {/* <svg className="cart"
           class="kadence-svg-icon kadence-shopping-bag-svg"
           fill="currentColor"
           version="1.1"
@@ -21,7 +21,28 @@ export default function CartLink() {
         >
           <title>Shopping Cart</title>
           <path d="M19 5h-14l1.5-2h11zM21.794 5.392l-2.994-3.992c-0.196-0.261-0.494-0.399-0.8-0.4h-12c-0.326 0-0.616 0.156-0.8 0.4l-2.994 3.992c-0.043 0.056-0.081 0.117-0.111 0.182-0.065 0.137-0.096 0.283-0.095 0.426v14c0 0.828 0.337 1.58 0.879 2.121s1.293 0.879 2.121 0.879h14c0.828 0 1.58-0.337 2.121-0.879s0.879-1.293 0.879-2.121v-14c0-0.219-0.071-0.422-0.189-0.585-0.004-0.005-0.007-0.010-0.011-0.015zM4 7h16v13c0 0.276-0.111 0.525-0.293 0.707s-0.431 0.293-0.707 0.293h-14c-0.276 0-0.525-0.111-0.707-0.293s-0.293-0.431-0.293-0.707zM15 10c0 0.829-0.335 1.577-0.879 2.121s-1.292 0.879-2.121 0.879-1.577-0.335-2.121-0.879-0.879-1.292-0.879-2.121c0-0.552-0.448-1-1-1s-1 0.448-1 1c0 1.38 0.561 2.632 1.464 3.536s2.156 1.464 3.536 1.464 2.632-0.561 3.536-1.464 1.464-2.156 1.464-3.536c0-0.552-0.448-1-1-1s-1 0.448-1 1z"></path>
-        </svg>
+        </svg> */}
+        <div class="preloader">
+	<svg class="cart" role="img" aria-label="Shopping cart line animation" viewBox="0 0 128 128" width="128px" height="128px" xmlns="http://www.w3.org/2000/svg">
+		<g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="8">
+			<g class="cart__track" stroke="hsla(0,10%,10%,0.1)">
+				<polyline points="4,4 21,4 26,22 124,22 112,64 35,64 39,80 106,80" />
+				<circle cx="43" cy="111" r="13" />
+				<circle cx="102" cy="111" r="13" />
+			</g>
+			<g class="cart__lines" stroke="currentColor">
+				<polyline class="cart__top" points="4,4 21,4 26,22 124,22 112,64 35,64 39,80 106,80" stroke-dasharray="338 338" stroke-dashoffset="-338" />
+				<g class="cart__wheel1" transform="rotate(-90,43,111)">
+					<circle class="cart__wheel-stroke" cx="43" cy="111" r="13" stroke-dasharray="81.68 81.68" stroke-dashoffset="81.68" />
+				</g>
+				<g class="cart__wheel2" transform="rotate(90,102,111)">
+					<circle class="cart__wheel-stroke" cx="102" cy="111" r="13" stroke-dasharray="81.68 81.68" stroke-dashoffset="81.68" />
+				</g>
+			</g>
+		</g>
+	</svg>
+
+</div>
         {total}
       </NavLink>
     </div>
