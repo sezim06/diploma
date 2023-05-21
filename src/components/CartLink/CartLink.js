36d400ba464a9ffd67221022be76265b63526637
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-21 21:36:15 +0600

    added and remove some code

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import "./CartLink.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+
 export default function CartLink() {
   const { cart } = useContext(AppContext);
   const total = Object.values(cart).reduce((ass, num) => ass + num, 0);
@@ -10,7 +11,6 @@ export default function CartLink() {
     <div className="CartLink">
       <NavLink to="/cart">
     
-       
        
         {total}
       </NavLink>
