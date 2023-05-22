11bdd26c6fa741848e117c70224cfba3f8c8268d
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-22 20:32:48 +0600

    add in js cart icon and add in css deleted height

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import "./CartLink.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
-
+import cartt from "./cart.png"
 export default function CartLink() {
   const { cart } = useContext(AppContext);
   const total = Object.values(cart).reduce((ass, num) => ass + num, 0);
@@ -10,7 +10,7 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <NavLink to="/cart">
-    
+    <img src={cartt} alt="cart" width={30} />
        
         {total}
       </NavLink>
