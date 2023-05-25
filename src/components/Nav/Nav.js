96a3d3bd213fcce299ead3d610b45f6b4b0fc0bc
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-25 18:14:26 +0600

    deleted  seach in drawer

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,35 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 import Search from "../Search/Search";
-export default function Nav(){
+export default function Nav() {
+  let menuBtn = document.querySelectorAll(".NavItem a");
+  let menu = document.querySelector(".Drawer");
+  menuBtn.forEach((el) => {
+    el.addEventListener("click", function () {
+      menu.classList.remove("open");
+    });
+  });
   return (
-<div className="Nav">
-<nav className="main-nav">
-      <ul>
-        <Search/>
-        <li className="NavItem">
-          <NavLink className="active" to="/">Home</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/books">Books</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/delivery">Delivery</NavLink>
-        </li>
-      </ul>
-      
-    </nav>
-</div>
-  )
+    <div className="Nav">
+      <nav className="main-nav">
+        <ul>
+          <Search />
+          <li className="NavItem">
+            <NavLink className="active" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="NavItem">
+            <NavLink to="/books">Books</NavLink>
+          </li>
+          <li className="NavItem">
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li className="NavItem">
+            <NavLink to="/delivery">Delivery</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
