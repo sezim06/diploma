3c871253096cf1ace606dd78147823bc2e9c14c5
sezim06 <sezimaliaskarova352@gmail.com>
2023-05-25 18:14:58 +0600

    changed orders and styled width

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -3,15 +3,18 @@ import { AppContext } from "../../App";
 import "./OrderList.css";
 
 export default function OrderList() {
+  // Получить заказы и продукты из контекста
   const { orders, products } = useContext(AppContext);
 
   if (!orders.length || !products.length) {
     return "No orders found.";
   }
 
-  const output = orders.map((order) => {
-    const cartOutput = Object.keys(order.cart).map((productId) => {
-      const product = products.find((product) => product.id === productId);
+  // Выводим все заказы.
+  const output = orders.map(order => {
+    // Вывести содержимое корзины для этого заказа
+    const cartOutput = Object.keys(order.cart).map(productId => {
+      const product = products.find(product => product.id === productId);
 
       if (!product) {
         return "Product not found";
@@ -19,23 +22,25 @@ export default function OrderList() {
 
       return (
         <div key={product.id}>
-          <img src={product.img} alt="product.name"/>
-          {product.name}: {order.cart[productId]} X {product.price} som ={" "}
-          {order.cart[productId] * product.price} som
+          <img src={product.picture} alt={product.name} />
+          {product.name}: {order.cart[productId]} X {product.price} som = {order.cart[productId] * product.price} som
         </div>
       );
-    });
+    })
 
     return (
       <div key={order.id} className="Order">
-        <div>Name: {order.name}</div>
-        <div>Phone: {order.phone}</div>
-        <div>Address: {order.address}</div>
-        <div>Cart: {cartOutput}</div>
+        <div><strong>Name</strong>: {order.name}</div>
+        <div><strong>Phone</strong>: {order.phone}</div>
+        <div><strong>Address</strong>: {order.address}</div>
+        <div><strong>Cart</strong>: {cartOutput}</div>
       </div>
     );
-  });
+  })
 
-  return <div className="OrderList">{output}</div>;
-
+  return (
+    <div className="OrderList">
+      {output}
+    </div>
+  );
 }
